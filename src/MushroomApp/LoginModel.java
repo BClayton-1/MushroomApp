18a46cd53cd833b2ccbca704967b1bcdb8ed5b96
BClayton-1 <bclaytonacademic@gmail.com>
2025-02-10 15:42:19 -0500

    Work on login model

--- src/MushroomApp/LoginModel.java
+++ src/MushroomApp/LoginModel.java
@@ -20,11 +20,28 @@ public class LoginModel {
 		}
 	}
 	
-	public boolean isLogin(String user, String password) {
-		PreparedStatement preparedStatement;
-		ResultSet resultSet;
+	public boolean isLogin(String username, String password) throws SQLException {
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
 		String query = "select * from user where username = ? and password = ?";
-		
+		try {
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setString(1, username);
+			preparedStatement.setString(2, password);
+			
+			resultSet = preparedStatement.executeQuery();
+			if (resultSet.next()) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		} catch (Exception e) {
+			return false;
+		} finally {
+			preparedStatement.close();
+			resultSet.close();
+		}
 	}
 
 }
